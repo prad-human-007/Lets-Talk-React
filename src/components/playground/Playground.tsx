a1f5ec2f9770ffb419f4682bb5bd4f4af4ae6525
prad-human-007 <kanasepradhumn@gmail.com>
2024-10-15 10:33:21 +0530

    Solved ES lint error

--- src/components/playground/Playground.tsx
+++ src/components/playground/Playground.tsx
@@ -109,6 +109,45 @@ export default function Playground({
 
   useDataChannel(onDataReceived);
 
+  const videoTileContent = useMemo(() => {
+    const videoFitClassName = `object-${config.video_fit || "cover"}`;
+
+    const disconnectedContent = (
+      <div className="flex items-center justify-center text-gray-700 text-center w-full h-full">
+        No video track. Connect to get started.
+      </div>
+    );
+
+    const loadingContent = (
+      <div className="flex flex-col items-center justify-center gap-2 text-gray-700 text-center h-full w-full">
+        <LoadingSVG />
+        Waiting for video track
+      </div>
+    );
+
+    const videoContent = (
+      <VideoTrack
+        trackRef={agentVideoTrack}
+        className={`absolute top-1/2 -translate-y-1/2 ${videoFitClassName} object-position-center w-full h-full`}
+      />
+    );
+
+    let content = null;
+    if (roomState === ConnectionState.Disconnected) {
+      content = disconnectedContent;
+    } else if (agentVideoTrack) {
+      content = videoContent;
+    } else {
+      content = loadingContent;
+    }
+
+    return (
+      <div className="flex flex-col w-full grow text-gray-950 bg-black rounded-sm border border-gray-800 relative">
+        {content}
+      </div>
+    );
+  }, [agentVideoTrack, config, roomState]);
+
   useEffect(() => {
     document.body.style.setProperty(
       "--lk-theme-color",
@@ -159,10 +198,210 @@ export default function Playground({
     return visualizerContent;
   }, [
     voiceAssistant.audioTrack,
-    config.settings.theme_color,
     roomState,
     voiceAssistant.state,
   ]);
+
+  const chatTileContent = useMemo(() => {
+    if (voiceAssistant.audioTrack) {
+      return (
+        <TranscriptionTile
+          agentAudioTrack={voiceAssistant.audioTrack}
+          accentColor={config.settings.theme_color}
+        />
+      );
+    }
+    return <></>;
+  }, [config.settings.theme_color, voiceAssistant.audioTrack]);
+
+  const settingsTileContent = useMemo(() => {
+    return (
+      <div className="flex flex-row gap-4 h-full w-full items-start overflow-y-auto">
+        {/* {config.description && (
+          <ConfigurationPanelItem title="Description">
+            {config.description}
+          </ConfigurationPanelItem>
+        )} */}
+
+        {/* <ConfigurationPanelItem title="Settings">
+          {localParticipant && (
+            <div className="flex flex-col gap-2">
+              <NameValueRow
+                name="Room"
+                value={name}
+                valueColor={`${config.settings.theme_color}-500`}
+              />
+              <NameValueRow
+                name="Participant"
+                value={localParticipant.identity}
+              />
+            </div>
+          )}
+        </ConfigurationPanelItem> */}
+        {/* <div>
+        <ConfigurationPanelItem title="Status">
+          <div className="flex flex-col gap-2">
+            <NameValueRow
+              name="Room connected"
+              value={
+                roomState === ConnectionState.Connecting ? (
+                  <LoadingSVG diameter={16} strokeWidth={2} />
+                ) : (
+                  roomState.toUpperCase()
+                )
+              }
+              valueColor={
+                roomState === ConnectionState.Connected
+                  ? `${config.settings.theme_color}-500`
+                  : "gray-500"
+              }
+            />
+            <NameValueRow
+              name="Agent connected"
+              value={
+                voiceAssistant.agent ? (
+                  "TRUE"
+                ) : roomState === ConnectionState.Connected ? (
+                  <LoadingSVG diameter={12} strokeWidth={2} />
+                ) : (
+                  "FALSE"
+                )
+              }
+              valueColor={
+                voiceAssistant.agent
+                  ? `${config.settings.theme_color}-500`
+                  : "gray-500"
+              }
+            />
+          </div>
+        </ConfigurationPanelItem>
+        </div> */}
+        {localVideoTrack && (
+          <ConfigurationPanelItem
+            title="Camera"
+            deviceSelectorKind="videoinput"
+          >
+            <div className="relative">
+              <VideoTrack
+                className="rounded-sm border border-gray-800 opacity-70 w-full"
+                trackRef={localVideoTrack}
+              />
+            </div>
+          </ConfigurationPanelItem>
+        )}
+        {localMicTrack && (
+          <ConfigurationPanelItem
+            title="Microphone"
+            deviceSelectorKind="audioinput"
+          >
+            <AudioInputTile trackRef={localMicTrack} />
+          </ConfigurationPanelItem>
+        )}
+        <ConfigurationPanelItem
+            title="">
+              <Button
+                accentColor={
+                  roomState === ConnectionState.Connected ? "red" : config.settings.theme_color
+                }
+                disabled={roomState === ConnectionState.Connecting}
+                onClick={() => {
+                  onConnect(roomState === ConnectionState.Disconnected);
+                }}
+              >
+                {roomState === ConnectionState.Connecting ? (
+                  <LoadingSVG />
+                ) : roomState === ConnectionState.Connected ? (
+                  "Disconnect"
+                ) : (
+                  "Connect"
+                )}
+              </Button>
+        </ConfigurationPanelItem>
+        {/* <div className="w-full">
+          <ConfigurationPanelItem title="Color">
+            <ColorPicker
+              colors={themeColors}
+              selectedColor={config.settings.theme_color}
+              onSelect={(color) => {
+                const userSettings = { ...config.settings };
+                userSettings.theme_color = color;
+                setUserSettings(userSettings);
+              }}
+            />
+          </ConfigurationPanelItem>
+        </div> */}
+        {config.show_qr && (
+          <div className="w-full">
+            <ConfigurationPanelItem title="QR Code">
+              <QRCodeSVG value={window.location.href} width="128" />
+            </ConfigurationPanelItem>
+          </div>
+        )}
+      </div>
+    );
+  }, [
+    config.description,
+    config.settings,
+    config.show_qr,
+    localParticipant,
+    name,
+    roomState,
+    localVideoTrack,
+    localMicTrack,
+    themeColors,
+    setUserSettings,
+    voiceAssistant.agent,
+  ]);
+
+  let mobileTabs: PlaygroundTab[] = [];
+  if (config.settings.outputs.video) {
+    mobileTabs.push({
+      title: "Video",
+      content: (
+        <PlaygroundTile
+          className="w-full h-full grow"
+          childrenClassName="justify-center"
+        >
+          {videoTileContent}
+        </PlaygroundTile>
+      ),
+    });
+  }
+
+  if (config.settings.outputs.audio) {
+    mobileTabs.push({
+      title: "Audio",
+      content: (
+        <PlaygroundTile
+          className="w-full h-full grow"
+          childrenClassName="justify-center"
+        >
+          {audioTileContent}
+        </PlaygroundTile>
+      ),
+    });
+  }
+
+  if (config.settings.chat) {
+    mobileTabs.push({
+      title: "Chat",
+      content: chatTileContent,
+    });
+  }
+
+  mobileTabs.push({
+    title: "Settings",
+    content: (
+      <PlaygroundTile
+        padding={false}
+        backgroundColor="gray-950"
+        className="h-full w-full basis-1/4 items-start overflow-y-auto flex"
+        childrenClassName="h-full grow items-start"
+      >
+        {settingsTileContent}
+      </PlaygroundTile>
+    ),
+  });
 
   return (
     <>
@@ -182,7 +421,7 @@ export default function Playground({
           
           <div className="flex flex-col py-6 gap-3 justify-around">
             <h1 className="text-4xl leading-tight text-center">
-              India's First Realtime Voice AI Teacher
+              India&apos;s First Realtime Voice AI Teacher
             </h1>
             <h1 className="text-l leading-tight text-center">
             Trained to give personalized tutoring to 
@@ -251,7 +490,7 @@ export default function Playground({
         </div>
         
         <div className="flex flex-row justify-center gap-3  mb-4">
-          <div className="flex items-center italic">
+          <div className="flex items-center ">
               <h1>
                 Made by Pradhumn &lt;3
               </h1>
